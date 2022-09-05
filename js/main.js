5646e7b669c9f64570a0cb5a01a6926bfcb79050
vaibhav-305 <mishravaibhav839@gmail.com>
2022-09-05 12:19:09 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-(function($) {
+(function ($) {
 
     var nav = $('nav');
     var navHeight = nav.outerHeight();
     // Smooth Scrolling of Nav links
-    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function() {
+    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
         var target = $(this.hash);
         $('html, body').animate({
-        scrollTop: (target.offset().top - navHeight + 5)
+            scrollTop: (target.offset().top - navHeight + 5)
         }, 1000, "easeInOutExpo");
     });
 
@@ -17,10 +17,10 @@
     });
 
     // Reduction of NavBar
-    var skills = $('#skills').offset().top-100;
-    var projects = $('#projects').offset().top-100;
+    var skills = $('#skills').offset().top - 100;
+    var projects = $('#projects').offset().top - 100;
     $(window).trigger('scroll');
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var pixels = 50;
         var top = 1200;
         if ($(window).scrollTop() > pixels) {
@@ -34,19 +34,17 @@
             $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
         } else {
             $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-        } 
-        if($(window).scrollTop() > skills && $(window).scrollTop() < projects)
-        {
+        }
+        if ($(window).scrollTop() > skills && $(window).scrollTop() < projects) {
             $('.navbar-expand-md').addClass('navbar-skill');
         }
-        else
-        {
+        else {
             $('.navbar-expand-md').removeClass('navbar-skill');
         }
     });
 
     // Typing Effect Settings
-    
+
     if ($('.text-slider').length == 1) {
         var typed_strings = $('.text-slider-items').text();
         var typed = new Typed('.text-slider', {
@@ -57,46 +55,53 @@
             backSpeed: 30
         });
     }
-    
+
     $(".slideshow1 > div:gt(0)").hide();
-    setInterval(function() { 
-    $('.slideshow1 > div:first')
-        .fadeOut(1000)
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('.slideshow1');
-    },  3000);
+    setInterval(function () {
+        $('.slideshow1 > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('.slideshow1');
+    }, 3000);
 
     $(".slideshow2 > div:gt(0)").hide();
-    setInterval(function() { 
-    $('.slideshow2 > div:first')
-        .fadeOut(1000)
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('.slideshow2');
-    },  3000);
+    setInterval(function () {
+        $('.slideshow2 > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('.slideshow2');
+    }, 3000);
 
-    
+
 })(jQuery);
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-  
+
     for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
     }
-  }
-  
-  window.addEventListener("scroll", reveal);
+}
+
+window.addEventListener("scroll", reveal);
+var preloader = document.getElementById('preloader');
+function removeLoader(){
+    preloader.style.opacity=0;
+    preloader.remove();
+}
+
+
 /*const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', ()=>{
